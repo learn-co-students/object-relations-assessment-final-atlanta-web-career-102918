4aa7e867ddfd9ca99dffb8b5eaa187dbc683c1c1
Tim Campbell <timothylevicampbell@gmail.com>
2018-09-28 21:14:25 -0500

    Add warning not to edit binding.pry to reduce bugs

--- tools/console.rb
+++ tools/console.rb
@@ -1,5 +1,7 @@
+# require and load the environment file
 require_relative '../config/environment.rb'
 
+# call this function instead of exiting and re-running this file
 def reload
   load 'config/environment.rb'
 end
@@ -8,5 +10,10 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+
+
+
+
+# ===== WARNING! DO NOT EDIT BELOW THIS LINE ===== #
 binding.pry
-0 #leave this here to ensure binding.pry isn't the last line
+0
